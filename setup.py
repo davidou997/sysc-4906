9952293a8f060197512c35b24ada78826bb8e85e
davidou997 <davidou997@gmail.com>
2022-03-13 17:18:05 -0400

    Initial cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='TBA',
+    author='Modal Canadian',
+    license='MIT',
+)
